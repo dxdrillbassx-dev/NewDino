a9518a599f762f3a2f040ce43b1991126dcfa824
sobr228 <askerka110038302@gmail.com>
2023-09-01 02:03:09 +0300

    0.0.2

--- src/main/java/git/dxdrillbassx/newdino/Game.java
+++ src/main/java/git/dxdrillbassx/newdino/Game.java
@@ -8,6 +8,10 @@ import org.bukkit.inventory.ItemStack;
 
 public class Game {
 
+    public static final Material jumpItem = Material.RED_CONCRETE;
+    public static final Material sneakItem = Material.GREEN_CONCRETE;
+    public static final Material dinoMaterial = Material.BLACK_WOOL;
+
     private Inventory field;
     private Player player;
 
@@ -26,5 +30,8 @@ public class Game {
         for (int i = 44; i < 54; i++){
             field.setItem(i, new ItemStack(ground));
         }
+
+        player.getInventory().setItem(0, new ItemStack(jumpItem));
+        player.getInventory().setItem(1, new ItemStack(sneakItem));
     }
 }
--- src/main/java/git/dxdrillbassx/newdino/events/JumpEvent.java
+++ src/main/java/git/dxdrillbassx/newdino/events/JumpEvent.java
@@ -1,4 +1,13 @@
 package git.dxdrillbassx.newdino.events;
 
-public class JumpEvent {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class JumpEvent implements Listener {
+
+    @EventHandler
+    public void onJump(InventoryClickEvent event){
+
+    }
 }
